6902971a5c69c68be1ae0057ee40c3f5af5cbdd8
tayef mahmud <mahmudovi183@gmail.com>
2024-09-28 09:00:08 +0600

    save

--- src/app/(client)/(user)/layout.jsx
+++ src/app/(client)/(user)/layout.jsx
@@ -1,3 +1,17 @@
-export default function Layout() {
-  return <div>Layout</div>;
+import { Container } from "@/components/container";
+import { Sidebar } from "@/components/sidebars/sidebar";
+
+export default function Layout({ children }) {
+  return (
+    <Container>
+      <div className="lg:flex">
+        {/* <ProfileSidebar data={userData} /> */}
+        <Sidebar />
+
+        <div className="w-full rounded-md">
+          <div className="p-0 md:p-2">{children}</div>
+        </div>
+      </div>
+    </Container>
+  );
 }
