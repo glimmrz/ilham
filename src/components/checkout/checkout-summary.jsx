b3eb3aed63a8412df22e502264b60f3550539ad2
tayef mahmud <mahmudovi183@gmail.com>
2024-10-01 13:01:16 +0600

    checkout summary heading

--- src/components/checkout/checkout-summary.jsx
+++ src/components/checkout/checkout-summary.jsx
@@ -1,15 +1,19 @@
 "use client";
 import { useCart } from "@/hooks/use-cart";
 import { CartItem } from "../cart/cart-item";
+import { Heading } from "../heading";
 
 export function CheckoutSummary() {
   const { cartItems } = useCart();
 
   return (
-    <div className="max-h-[600px] overflow-auto grid gap-2 mb-2 md:sticky md:top-[calc(theme(height.16)+theme(height.14)+theme(gap.2))]">
-      {cartItems?.map((item, index) => (
-        <CartItem key={index} item={item} />
-      ))}
-    </div>
+    <>
+      <Heading>order summary</Heading>
+      <div className="mt-4 max-h-[600px] overflow-auto grid gap-2 mb-2 md:sticky md:top-[calc(theme(height.16)+theme(height.14)+theme(gap.2))]">
+        {cartItems?.map((item, index) => (
+          <CartItem key={index} item={item} />
+        ))}
+      </div>
+    </>
   );
 }
