e43791f379b79b75f785e6f3af82f94e478e478f
tayef mahmud <mahmudovi183@gmail.com>
2024-10-01 11:54:20 +0600

    proper logo

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export const Logo = () => {
   return (
     <Link href="/">
-      <div className="h-12 w-36 bg-red-100">
-        <h1></h1>
+      <div className="h-12 w-36 flex items-center justify-center">
+        <h1 className="text-2xl text-center font-extrabold">iLHAM</h1>
       </div>
     </Link>
   );
