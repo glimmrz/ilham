4d49f382d1aa9d567c307c4555a3afa2c9f482d1
tayef mahmud <mahmudovi183@gmail.com>
2024-10-02 09:17:00 +0600

    google verification tag

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,10 @@
-import { ThemeProvider } from "@/components/providers/theme-provider";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 import { font } from "@/lib/fonts";
 import { Toaster } from "@/components/ui/sonner";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
-import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export const metadata = {
   title: {
@@ -24,6 +24,9 @@ export const metadata = {
   openGraph: {},
   description:
     "Discover a wide range of eco-friendly jute bags for men and women, premium grocery items, and elegant home decor at iLHAM. Our sustainable jute products are perfect for fashion-conscious individuals who care about the environment. Shop high-quality ladies' handbags, men's bags, and essential grocery items along with stylish home decor pieces that elevate your living space. Enjoy fast delivery, exclusive offers, and unbeatable prices. Shop now for the best in sustainable fashion and home essentials.",
+  verification: {
+    google: "PdOkfv46dUopcBct9pLHm0Gz98vRuoiPEA52kdAxexk",
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -34,8 +37,7 @@ export default function RootLayout({ children }) {
           {children}
           <Toaster />
         </ThemeProvider>
-        <GoogleTagManager gtmId="GTM-NHPCWCXZ" />
-        <GoogleAnalytics gaId="GTM-NHPCWCXZ" />
+        <GoogleTagManager gtmId="GTM-W3ST2L6G" />
         <SpeedInsights />
         <Analytics />
       </body>
