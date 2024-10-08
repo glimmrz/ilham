87a7ba5f0516d6cbc68c1e92261a3c54d0a28682
tayef mahmud <mahmudovi183@gmail.com>
2024-10-08 23:30:17 +0600

    promo slider rm from category page

--- src/app/(client)/category/[category]/page.jsx
+++ src/app/(client)/category/[category]/page.jsx
@@ -2,7 +2,6 @@ import { CategoryView } from "@/components/category-view";
 import { Container } from "@/components/wrappers/container";
 import { Product } from "@/components/product-cards/product";
 import { ProductView } from "@/components/product-view";
-import { PromoSlider } from "@/components/promo-slider";
 import { getData } from "@/utils/api-calls";
 import { Suspense } from "react";
 import { ProductviewSkeleton } from "@/components/skeletons/productview-skeleton";
