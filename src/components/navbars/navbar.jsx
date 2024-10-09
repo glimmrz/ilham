d1323eb9506bf78ede8063ea817442e9d2c13d52
tayef mahmud <mahmudovi183@gmail.com>
2024-10-09 15:02:18 +0600

    disabled search

--- src/components/navbars/navbar.jsx
+++ src/components/navbars/navbar.jsx
@@ -19,14 +19,14 @@ export const Navbar = ({ userData }) => {
           <Logo />
 
           <div className="flex items-center gap-4">
-            <Button
+            {/* <Button
               variant="outline"
               size="icon"
               icon="search"
               className="rounded-full"
             >
               <span className="sr-only">search</span>
-            </Button>
+            </Button> */}
 
             <Button
               icon="menu"
