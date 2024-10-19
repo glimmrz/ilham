07771651d12a297902dc538da32ca1f7b40ffc00
tayef mahmud <mahmudovi183@gmail.com>
2024-10-19 21:40:32 +0600

    font fix

--- src/lib/fonts.js
+++ src/lib/fonts.js
@@ -1,7 +1,7 @@
 import { Quicksand } from "next/font/google";
 
 export const font = Quicksand({
-  weights: [400, 500, 600, 700],
+  weight: ["300", "400", "500", "600", "700"],
   styles: ["normal", "italic"],
   subsets: ["latin-ext"],
 });
